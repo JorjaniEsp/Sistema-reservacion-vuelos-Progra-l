ec4418a2f51d853bb2a065f667bb2c7a58748712
JorjaniEsp <espinozajorjanie2006@gmail.com>
2025-10-27 15:04:56 -0600

    prueba

---
+++ Proyect/src/domain/Vuelo.java
@@ -0,0 +1,8 @@
+
+package domain;
+
+public class Vuelo {
+    
+    private String id;
+    
+}
---
+++ Proyect/src/presentation/FmrMenuPrincipal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package presentation;
+
+/**
+ *
+ * @author josse
+ */
+public class FmrMenuPrincipal {
+    
+}
