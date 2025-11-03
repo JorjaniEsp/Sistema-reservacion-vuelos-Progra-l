b869073396f26b599a34e0b1fae546475111708f
unknown <justinf10educ@gmail.com>
2025-11-03 11:41:30 -0600

    Add añadí atributos y métodos a la clase avión y tiquete

--- Proyect/src/domain/Tiquete.java
+++ Proyect/src/domain/Tiquete.java
@@ -3,4 +3,29 @@ package domain;
 
 public class Tiquete {
     
+    private Reservacion reservacion;
+    
+    public Tiquete(Reservacion reservacion){
+        this.reservacion = reservacion;
+    }
+    
+    public String mostrar(){
+        return this.toString();
+    }
+    @Override
+    public String toString(){
+    return "=======================\n" +
+           "     TIQUETE EMITIDO   \n" +
+           "=======================\n" +
+           "Detalles de la Reservación: \n" +
+            reservacion.toString() + "\n" +
+           "------------------------------------\n" +
+           "Gracias por su compra\n" +
+           "============================";
+    }
+    
+    public Reservacion getReservacion(){
+        return reservacion;
+    }
+    
 }
