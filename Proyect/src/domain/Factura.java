9629271fb54e1078e1c7259cabcc0e7cd5aaed3f
Angely Perez <guidoangely1987@gmail.com>
2025-11-05 19:28:58 -0600

    Add(domain.pasajero y domain.factura): añadi atributos y metodos

--- Proyect/src/domain/Factura.java
+++ Proyect/src/domain/Factura.java
@@ -1,5 +1,51 @@
 package domain;
 
-public class Factura {
+public class Factura extends Documento {
     
+    private Reservacion reservacion;
+    private MetodoPago metodoPago;
+
+    public Factura() {
+    }
+
+    public Factura(Reservacion reservacion, MetodoPago metodoPago) {
+        this.reservacion = reservacion;
+        this.metodoPago = metodoPago;
+    }
+
+    public Factura(Reservacion reservacion, MetodoPago metodoPago, int idDocumento, String fechaEmision) {
+        super(idDocumento, fechaEmision);
+        this.reservacion = reservacion;
+        this.metodoPago = metodoPago;
+    }
+
+    public Reservacion getReservacion() {
+        return reservacion;
+    }
+
+    public void setReservacion(Reservacion reservacion) {
+        this.reservacion = reservacion;
+    }
+
+    public MetodoPago getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(MetodoPago metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+    
+    public String mostrar() {
+    return "";
+    }
+
+    @Override
+    public String toString() {
+        return "---Informacion de factura---" +
+                "ID documento: " + getIdDocumento() +
+                "Fecha de emisión: " + getFechaEmision() +
+                "Reservacion: " + reservacion +
+                "Metodo de pago: " + metodoPago;
+    }
+   
 }
